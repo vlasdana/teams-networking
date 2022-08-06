5accfde4de44aa1f35bd4581fd1c29a12c7e2270
Dana Monica Vlas <dana.vlas87@gmail.com>
2022-08-06 15:45:36 +0200

    extract get HTML function

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+function getTeamHTML(team) {
+  return `
+  <tr>
+     <td>${team.promotion}</td>
+     <td>${team.members}</td>
+     <td>${team.name}</td>
+     <td>
+     <a href="${team.url}">open</a>
+     </td>
+     <td>x e</td>
+ </tr>`;
+}
 function displayTeams(teams) {
-  //   transorma in html
+  //   transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
-    teamsHTML += `
-     <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>
-        <a href="${team.url}">open</a>
-        </td>
-        <td>x e</td>
-    </tr>`;
-
-    // afisare
-    document.querySelector("table tbody").innerHTML = teamsHTML;
+    teamsHTML += getTeamHTML(team);
   });
+  // afisare
+  document.querySelector("table tbody").innerHTML = teamsHTML;
 }
 
 function loadTeams() {
