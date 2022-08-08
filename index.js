adb39bd2cdc345da04a6670a55b761e51c599fbe
Dana Monica Vlas <dana.vlas87@gmail.com>
2022-08-08 19:41:12 +0200

    prepare submit team

--- index.js
+++ index.js
@@ -28,4 +28,28 @@ function loadTeams() {
     });
 }
 
+function submitForm(e) {
+  e.preventDefault();
+  var promotion = document.querySelector("input[name=promotion]").value;
+  var members = document.querySelector("input[name=members]").value;
+  var name = document.querySelector("input[name=name]").value;
+  var url = document.querySelector("input[name=url]").value;
+  var team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+  console.warn("submit", JSON.stringify(team));
+
+  console.warn("submit", promotion, members, name, url);
+}
+
+function initEvents() {
+  var form = document.getElementById("editForm");
+
+  form.addEventListener("submit", submitForm);
+}
+
 loadTeams();
+initEvents();
